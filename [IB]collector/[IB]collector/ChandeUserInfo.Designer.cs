b8d644c184fe43ad1f2bcb2533429978d2125f23
igorBobrovA <57355113+igorBobrovA@users.noreply.github.com>
2020-03-05 18:03:06 +0700

    Revert "Revert "v0.1.8""
    This reverts commit 86dded55e65d1380d024762aa3678d25cff95ebc.

--- [IB]collector/[IB]collector/ChandeUserInfo.Designer.cs
+++ [IB]collector/[IB]collector/ChandeUserInfo.Designer.cs
@@ -37,6 +37,8 @@
             this.label4 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -74,7 +76,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 163);
+            this.label3.Location = new System.Drawing.Point(326, 13);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(78, 31);
             this.label3.TabIndex = 4;
@@ -82,7 +84,7 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(19, 197);
+            this.textBox3.Location = new System.Drawing.Point(332, 47);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(304, 38);
             this.textBox3.TabIndex = 5;
@@ -90,7 +92,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(13, 238);
+            this.label4.Location = new System.Drawing.Point(326, 88);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(81, 31);
             this.label4.TabIndex = 6;
@@ -102,7 +104,7 @@
             this.comboBox1.Items.AddRange(new object[] {
             "a",
             "u"});
-            this.comboBox1.Location = new System.Drawing.Point(19, 272);
+            this.comboBox1.Location = new System.Drawing.Point(332, 121);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(304, 39);
             this.comboBox1.TabIndex = 7;
@@ -110,18 +112,40 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(19, 317);
+            this.checkBox1.Location = new System.Drawing.Point(19, 166);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(78, 35);
             this.checkBox1.TabIndex = 8;
             this.checkBox1.Text = "ban";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(304, 47);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Сохранить изменения";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(332, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(304, 47);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Отменить изменения";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // ChandeUserInfo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(333, 358);
+            this.ClientSize = new System.Drawing.Size(647, 261);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label4);
@@ -152,5 +176,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- [IB]collector/[IB]collector/ChandeUserInfo.cs
+++ [IB]collector/[IB]collector/ChandeUserInfo.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace _IB_collector
 {
@@ -23,5 +24,28 @@ namespace _IB_collector
             this.id = id;
         }
         string id;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SQLiteConnection con = new SQLiteConnection(@"DataSource=Users/Users_info.db;Version=3;");
+            SQLiteCommand cmd = new SQLiteCommand();
+            cmd.Connection = con;
+            con.Open();
+            cmd.CommandText = "UPDATE Users " +
+                              "SET login = '" + textBox1.Text + "', " +
+                              "password = '" + textBox2.Text + "', " +
+                              "email = '" + textBox3.Text + "', " +
+                              "rights = '" + comboBox1.Text +"', " +
+                              "ban = '" + checkBox1.Checked + "' " +
+                              "WHERE id = " + id;
+            cmd.ExecuteNonQuery();
+            cmd.Reset();
+            con.Close();
+        }
     }
 }
